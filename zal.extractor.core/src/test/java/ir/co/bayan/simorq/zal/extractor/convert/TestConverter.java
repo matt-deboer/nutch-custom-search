9f229af0942c58b4b8ea0dc5f47a794c9beacc30
matt-deboer <matt.deboer@gmail.com>
2015-06-26 22:42:33 -0700

    Added missing TestConverter class to fix build

---
+++ zal.extractor.core/src/test/java/ir/co/bayan/simorq/zal/extractor/convert/TestConverter.java
@@ -0,0 +1,10 @@
+package ir.co.bayan.simorq.zal.extractor.convert;
+
+public class TestConverter implements Converter {
+
+	@Override
+	public Object convert(String value) {
+		return value;
+	}
+
+}
